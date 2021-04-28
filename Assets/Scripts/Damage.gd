220e862f7a3292a64670259908a4e0d6fc92e4fa
gavin <adstarr6@gmaill.com>
2021-04-28 16:23:35 -0700

    First attempt at adding the damage system

--- Assets/Scripts/Damage.gd
+++ Assets/Scripts/Damage.gd
@@ -3,18 +3,15 @@ extends Area2D
 var can_interact = false
 var is_on = true
 
-onready var sprite = get_node("AnimatedSprite")
-onready var SFX = get_node("SFX")
-
 func _ready():
-	sprite.frame = 0
+	pass
 
-func _on_Power_Switch_body_entered(body):
+func _on_Damage_body_entered(body):
 	if body.name == "Player":
 		print("Can interact")
 		can_interact = true
 
-func _on_Power_Switch_body_exited(body):
+func _on_Damage_body_exited(body):
 	if body.name == "Player":
 		print("Can no longer interact")
 		can_interact = false
@@ -23,11 +20,7 @@ func _input(_event):
 	if Input.is_action_just_pressed("interact") and can_interact == true:
 		print("Attempting to interact")
 		
-		SFX.play()
-		
-		if is_on == true:
-			is_on = false
-			sprite.frame = 1
-		else:
-			is_on = true
-			sprite.frame = 0
+		var root = get_tree().current_scene
+		var player = root.find_node("PlayerStats")
+		print(player.hp)
+		player.hp -= 3
