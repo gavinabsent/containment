5ad01928cd4bdad02533a2af2ad9a1b077d9f722
gavin <adstarr6@gmaill.com>
2021-05-04 11:47:49 -0700

    Update doors, start on new map, and other small things

--- Assets/Scripts/Normal Door Bottom.gd
+++ Assets/Scripts/Normal Door Bottom.gd
@@ -2,24 +2,19 @@ extends Area2D
 
 export(int) var next_position_x
 export(int) var next_position_y
-export(bool) var is_bottom = false
-
 var can_interact = false
-var offset = 0
 onready var root = get_tree().get_root().get_node("Game")
 onready var Player = root.find_node("Player")
 
-
 func _ready():
-	if is_bottom == true:
-		offset = 64
+	pass 
 
-func _on_Door_body_entered(body):
+func _on_Normal_Door_Bottom_body_entered(body):
 	if body.name == "Player":
 		print("Can interact")
 		can_interact = true
 
-func _on_Door_body_exited(body):
+func _on_Normal_Door_Bottom_body_exited(body):
 	if body.name == "Player":
 		print("Can no longer interact")
 		can_interact = false
@@ -28,8 +23,8 @@ func _input(_event):
 	if Input.is_action_just_pressed("interact") and can_interact == true:
 		print("Attempting to interact")
 		
-		var position_offset_x = Player.position.x - position.x
-		var position_offset_y = Player.position.y - position.y
+		var position_offset_x = Player.global_position.x - position.x
+		var position_offset_y = Player.global_position.y - position.y
 		
-		Player.global_position = Vector2(next_position_x + position_offset_x, next_position_y + position_offset_y - 32 + offset)
+		Player.global_position = Vector2(next_position_x + position_offset_x, next_position_y + position_offset_y + 32)
 		get_node("SFX").play()
--- Assets/Scripts/Normal Door Top.gd
+++ Assets/Scripts/Normal Door Top.gd
@@ -3,18 +3,18 @@ extends Area2D
 export(int) var next_position_x
 export(int) var next_position_y
 var can_interact = false
-onready var root = get_parent().get_parent()
+onready var root = get_tree().get_root().get_node("Game")
 onready var Player = root.find_node("Player")
 
 func _ready():
 	pass 
 
-func _on_BottomDoor_body_entered(body):
+func _on_Normal_Door_Top_body_entered(body):
 	if body.name == "Player":
 		print("Can interact")
 		can_interact = true
 
-func _on_BottomDoor_body_exited(body):
+func _on_Normal_Door_Top_body_exited(body):
 	if body.name == "Player":
 		print("Can no longer interact")
 		can_interact = false
@@ -23,8 +23,10 @@ func _input(_event):
 	if Input.is_action_just_pressed("interact") and can_interact == true:
 		print("Attempting to interact")
 		
-		var position_offset_x = Player.global_position.x - position.x
-		var position_offset_y = Player.global_position.y - position.y
+		var position_offset_x = Player.position.x - position.x
+		var position_offset_y = Player.position.y - position.y
 		
-		Player.global_position = Vector2(next_position_x + position_offset_x, next_position_y + position_offset_y + 32)
+		Player.global_position = Vector2(next_position_x + position_offset_x, next_position_y + position_offset_y - 32)
 		get_node("SFX").play()
+
+
